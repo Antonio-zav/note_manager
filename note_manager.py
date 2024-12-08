657e233f5b83840a0eafbef4cf0aaff4c03fa319
антон <ant400@ro.ru>
2024-12-08 13:42:10 +0300

    08/12

--- note_manager.py
+++ note_manager.py
@@ -1,11 +1,14 @@
 # grade 1. Этап 1. Задание 2
 
-from datetime import datetime
-from greetings import issue_date
+from add_input import issue_date
+from add_input import created_date
 
-issue_date = input('Введите дату в формате ЧЧ.ММ.ГГГГ: ')
-print(issue_date)
+created_date_d = int(created_date[0:2])
+created_date_m = int(created_date[3:5])
+created_date_y = int(created_date[6:])
+print('Сегодняшняя дата:', created_date_d, '.', created_date_m)    #Вывод текущей даты без вывода года
+
 issue_date_d = int(issue_date[0:2])
 issue_date_m = int(issue_date[3:5])
 issue_date_y = int(issue_date[6:])
-print('Введенная дата:', issue_date_d, '.', issue_date_m)
+print('Нужная дата:', issue_date_d, '.', issue_date_m)        #Вывод нужной даты без вывода года
