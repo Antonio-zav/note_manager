3c8f4dd502b31359a6e20fdb22af1b4f1d2f6044
antonio-zav <ant400@ro.ru>
2024-12-27 21:43:42 +0300

    27/12

--- check_deadline.py
+++ check_deadline.py
@@ -1,9 +1,16 @@
-# grade 1. Этап 2. Задание 3
+# # grade 1. Этап 2. Задание 3
 
-from datetime import date
+from datetime import date, datetime
 
 current_date = str(date.today())
-issue_date = input('Введите нужную дату в формате ЧЧ.ММ.ГГГГ: ')
+while True:
+    issue_date = input('Введите нужную дату в формате ЧЧ.ММ.ГГГГ: ')
+    format = "%d.%m.%Y"
+    try:
+        res = bool(datetime.strptime(issue_date, format))
+        break
+    except ValueError:
+        print ('Вы ввели некоректный формат даты!')
 
 current_date_d= int(current_date[8:])
 current_date_m = int(current_date[5:7])
@@ -13,13 +20,13 @@ issue_date_d = int(issue_date[0:2])
 issue_date_m = int(issue_date[3:5])
 issue_date_y = int(issue_date[6:])
 
-if current_date_d < issue_date_d and current_date_m <= issue_date_m:
-    print('До окончаию заметки ещё осталось время! ')
+if current_date_d < issue_date_d and current_date_m <= issue_date_m and current_date_y <= issue_date_y:
+    print(f'До окончаию заметки ещё осталось {issue_date_d - current_date_d} дней! ')
 elif current_date_d == issue_date_d and current_date_m == issue_date_m:
     print('Срок заметки окончится сегодня!!!')
 else:
     print('Срок вашей заметки истек', current_date_d - issue_date_d, 'дней назад')
-    print(issue_date, '- Дата создания заметки')
-    print(current_date, '- Текущая дата')
+print(issue_date, '- Дата создания заметки')
+print(current_date, '- Текущая дата')
 
 
